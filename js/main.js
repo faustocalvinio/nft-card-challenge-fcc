f2c826b33e8c90016cfd3c3471ce375a850c7d70
faustocalvinio <faustocalvino@outlook.com>
2022-02-25 17:26:46 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const qs=(a)=>document.querySelector(a)
 
 const imgEquilibrum=qs('.img-eq');
 const imgView=qs('.container-view')
+const blueBgDiv=qs('.blue-bg')
+const imgEye=qs('.img-view')
 
 imgEquilibrum.addEventListener('mouseover',()=>{
    imgView.classList.toggle('active',true);
@@ -10,3 +12,19 @@ imgEquilibrum.addEventListener('mouseover',()=>{
 imgEquilibrum.addEventListener('mouseleave',()=>{
     imgView.classList.remove('active');
 })
+blueBgDiv.addEventListener('mouseover',()=>{
+    imgView.classList.toggle('active',true);
+ });
+ 
+blueBgDiv.addEventListener('mouseleave',()=>{
+    imgView.classList.remove('active');
+ })
+
+imgEye.addEventListener('mouseover',()=>{
+    imgView.classList.toggle('active',true);
+ });
+ 
+imgEye.addEventListener('mouseleave',()=>{
+     imgView.classList.remove('active');
+ })
+
